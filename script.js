3ed06399ea2d1d4e2fe8e8dbe65bde009bfcd3a8
Malik Kotb <malikkotb@icloud.com>
2024-03-17 21:52:46 +0100

    mask ui animation

--- script.js
+++ script.js
@@ -3,13 +3,11 @@ const insanityBtn = document.getElementById("insanityBtn");
 const list = document.getElementById("list");
 
 discoverBtn.addEventListener("click", function () {
-//   list.style.display = "flex";
-  list.style.transform = "translateX(300px)";
+  list.style.transform = "translateX(0)";
   discoverBtn.style.transform = "translateX(300px)";
 });
 
 insanityBtn.addEventListener("click", function () {
-//   list.style.display = "none";
-  list.style.transform = "translateX(0)";
+  list.style.transform = "translateX(-350px)";
   discoverBtn.style.transform = "translateX(0)";
 });
